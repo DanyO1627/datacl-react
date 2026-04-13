b1adaf4de696748ccf9c7eadce2a5c686793a36b
Dany01627 <dani.oliveros@duocuc.cl>
2026-04-12 21:34:24 -0400

    Agrego pantallas Login y Registro con sus respectivos CSS

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.basededatos import engine, Base
 from app import models
-from app.routers import auth 
+from app.routers import auth
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="DataCL API")
+
+# Permite que React (puerto 5173) llame al backend (puerto 8000)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(auth.router)
 
 @app.get("/")
 def root():
-    return {"mensaje:": "DataCL API funcionando"}
-
+    return {"mensaje": "DataCL API funcionando"}
 # para ejecutar : uvicorn main:app --reload
 
 # Backend (en otra terminal)
