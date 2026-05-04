7b04b8ea6c2443adfa6b593f6dba0849c2de6938
Constanza Raquel Pino Villa <co.pinov@duocuc.cl>
2026-05-03 23:59:57 -0400

    se realiza cambio para que se lea el token y se acepte la descarga del informe por parte del usuario

--- frontend/src/services/informesService.js
+++ frontend/src/services/informesService.js
@@ -1,20 +1,30 @@
+import axios from 'axios'
+
 const API = 'http://localhost:8000'
 
-export async function obtenerInformes(token) {
-  const res = await fetch(`${API}/informes`, {
-    headers: { Authorization: `Bearer ${token}` },
-  })
-  if (!res.ok) throw new Error('Error al obtener informes')
-  return res.json()
+const api = axios.create({ baseURL: API })
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token')
+  if (token) config.headers.Authorization = `Bearer ${token}`
+  return config
+})
+
+export async function obtenerInformes() {
+  const res = await api.get('/informes')
+  return res.data
 }
 
-export function descargarInforme(id, token) {
-  const url = `${API}/informes/${id}/descargar`
+export async function descargarInforme(id) {
+  const respuesta = await api.get(`/informes/${id}/descargar`, {
+    responseType: 'blob'
+  })
+  const url = window.URL.createObjectURL(new Blob([respuesta.data]))
   const link = document.createElement('a')
   link.href = url
-  link.target = '_blank'
-  link.rel = 'noopener noreferrer'
+  link.setAttribute('download', `informe_${id}.pdf`)
   document.body.appendChild(link)
   link.click()
-  document.body.removeChild(link)
+  link.remove()
+  window.URL.revokeObjectURL(url)
 }
